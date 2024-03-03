416c51d52c251b3745618d726fba6068c15f1d1d
Lakshay <lakshay.sbp@gmail.com>
2024-03-02 23:07:00 -0500

    added header

--- ux-ui_resp_frontend/src/App.jsx
+++ ux-ui_resp_frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <h1 className="text-5xl text-sky-500">Hello World</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
       <ButtonGradient />
     </>
   );
